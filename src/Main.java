a3917e8bb572a4bebc9380698923a05516db52d8
ibrakhimk <kasymov_i@iuca.kg>
2023-03-18 22:12:48 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero();
+        Boss noBoss = new Boss();
+        Boss Hulk = new Boss();
+        Boss Spider = new Boss();
+        }
+}
